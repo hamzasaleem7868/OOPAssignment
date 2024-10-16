0057c8546fe22cfeb99a35a6500087129cee02e2
hamzasaleem7868 <mhamzasaleem7868@gmail.com>
2024-10-16 22:45:22 +0500

    Add files via upload
    My first code commited

---
+++ Assignment.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class student
+{
+    public:
+    	
+    student()
+	{
+    cout<<"student name is asad"<<endl;
+    }
+    void temp(void) 
+	{
+    cout<<"wellcome asad"<<endl;
+    }
+};
+    main() 
+{
+    student x ,y;
+};
